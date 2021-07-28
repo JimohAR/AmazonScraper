82c9faac36c6e8963906e15dc394e9d58f054abd
JimohAR <abolarinwarildwan@gmail.com>
2021-07-28 13:03:21 +0100

    added get_keepa_link to mvp

--- notebooks/Main.py
+++ notebooks/Main.py
@@ -11,7 +11,7 @@ class AmazonScrapper:
     It searches and stores the products that fit in the following criteria:
     - Has only One Seller
     - Not in the blacklisted Brandlist given
-    - BRN number within the range of 1 to 150,000
+    - bsr number within the range of 1 to 150,000
 
     """
     
@@ -25,62 +25,78 @@ class AmazonScrapper:
         
         """
         self.driver_path = driver_path
-        self.brand_list_path = brand_list_path
         self.base_url = base_url
         self.no_of_pages = no_of_pages
         self.paging_url = paging_url
-        self.proc_paging_url()
+        self.proc_urls()
+        self.set_brand_list(brand_list_path)
         
-    def proc_paging_url(self):
+    def proc_urls(self):
         """
         The paging url is formatted so it can be reproducable for multiple pages
         
         """
-        self.paging_url = self.paging_url.replace(re.findall('qid=\d+&*', self.paging_url)[0], '')
+        try:
+            self.base_url = self.base_url.replace(re.findall('qid=\d+&*', self.paging_url)[0], '')
+        except IndexError:
+            pass
+        try:
+            self.paging_url = self.paging_url.replace(re.findall('qid=\d+&*', self.paging_url)[0], '')
+        except IndexError:
+            pass
         self.paging_url = self.paging_url.replace(re.findall('page=\d', self.paging_url)[0], 'page={}')
         self.paging_url = self.paging_url.replace(re.findall('sr_pg_\d', self.paging_url)[0], 'sr_pg_{}')
+        
+    def set_brand_list(self, path):
+        """
+        Makes the Black listed brand lists
+        
+        """
+        self.brand_list = pd.read_csv(path).set_index('0')#.iloc[:,0].to_list()
     
-    @property
-    def brand_list(self):
-        """
-        Returns the Black listed brand lists
-        
-        """
-        return pd.read_csv(self.brand_list_path).set_index('0')#.iloc[:,0].to_list()
-        
-    def get_paging_url(self, page_no):
-        """
-        Gets the paging url from the user
-        
-        """
-        return self.paging_url.format(page_no, page_no)
-    
-    def set_src(self, page_no=1):
+    def set_src(self, page_no=1, err = 1):
         """ 
         Sets the HTML source code of the page in class variabe src
         
         """
-        options = Options()
-        # connects the driver to the already opened browser with remote debugging
-        options.add_experimental_option('debuggerAddress', 'localhost:9222')
+        try:
+            options = Options()
+            # connects the driver to the already opened browser with remote debugging
+            options.add_experimental_option('debuggerAddress', 'localhost:9222')
 
-        driver = webdriver.Chrome(options=options, executable_path=self.driver_path)
-        if page_no == 1:
-            driver.get(self.base_url)
-        elif page_no > 1:
-            driver.get(self.get_paging_url(page_no))
-        
-        self.src = bs(driver.page_source, features= 'html.parser')
-        driver.quit()
+            driver = webdriver.Chrome(options=options, executable_path=self.driver_path)
+            if page_no == 1:
+                driver.get(self.base_url)
+            elif page_no > 1:
+                driver.get(self.get_paging_url(page_no))
+
+            self.src = bs(driver.page_source, features= 'html.parser')
+            self.set_product_panes()
+            driver.quit()
+        except:
+            if err <= 3:
+                time.sleep(2)
+                self.set_src(page_no, err = err + 1)
+                if err == 3:
+                    print('couldn\'t get src')
+            return ''
+            
     
-    @property
-    def product_panes(self):
+    def set_product_panes(self):
         """
         Returns the product holders' source codes
         
         """
-        product_panes = self.src.find_all('div', {'data-component-type':'s-search-result'})
-        return product_panes
+        self.product_panes = self.src.find_all('div', {'data-component-type':'s-search-result'})
+        
+        
+         
+    def get_paging_url(self, page_no):
+        """
+        Gets the paging url from the user
+
+        """
+        return self.paging_url.format(page_no, page_no)
 
     def get_product_asin(self, product_pane):
         """
@@ -116,16 +132,23 @@ class AmazonScrapper:
         product_link = 'https://www.amazon.com' + product_pane.h2.a['href']
         return product_link
     
-    def get_ccc_link(product_pane):
-        f = 'https://charts.camelcamelcamel.com/us/{}/amazon.png?force=1&zero=0&w=594&h=356&desired=false&legend=1&ilt=1&tp={}&fo=1&lang=en'
-        
+    def get_keepa_link(self, product_pane):
+        f = 'https://keepa.com/#!product/1-{}'
         asin = self.get_product_asin(product_pane)
-        all_ccc = f.format(asin, 'all')
+        
+        keepa_link = f.format(asin)
+        return keepa_link
+    
+    def get_ccc_links(self, product_pane):
+        f = 'https://charts.camelcamelcamel.com/us/{}/amazon.png?force=1&zero=0&w=594&h=356&desired=false&legend=1&ilt=1&tp={}&fo=1&lang=en'
+        f1 = 'https://charts.camelcamelcamel.com/us/{}/amazon-new.png?force=1&zero=0&w=594&h=356&desired=false&legend=1&ilt=1&tp={}&fo=1&lang=en'
+        asin = self.get_product_asin(product_pane)
+        
         m_ccc = f.format(asin, '3m')
+        all_ccc = f1.format(asin, 'all')
         
         return all_ccc, m_ccc
         
-    
     def get_product_price(self, product_pane):
         """
         Gets the product price
@@ -166,7 +189,7 @@ class AmazonScrapper:
         
         """
         price = self.get_product_price(product_pane)
-        if price == -1:
+        if (price == -1):
             return 0
         else:
             return 1
@@ -177,42 +200,37 @@ class AmazonScrapper:
         if it passes return 1, otherwise 0 (helium extension used)
         
         """
+        helium_pane = self.get_helium_pane(product_pane)
         try:
-            helium_pane = self.get_helium_pane(product_pane)
-            try:
-                seller_count = helium_pane.findAll('div', recursive= False)[1].findAll('div')[1].findAll('a')[1].text.split(' ')[0].replace(',', '')
-            except IndexError:
-                print(IndexError)
-                return 0
-            
-            if int(seller_count) > count:
-                return 0
-            else:
-                return 1
-        except err:
-            print(f'filter_asin_by_seller_count ==> {err}')
+            seller_count = helium_pane.findAll('div', recursive= False)[1].findAll('div', recursive= False)[1].findAll('a')[1].text.split(' ')[0].replace(',', '')
+        except IndexError:
+#                 print(IndexError)
             return 0
 
-    def filter_asin_by_brn_no(self, product_pane):
+        if int(seller_count) > count:
+            return 0
+        else:
+            return 1
+
+
+    def filter_asin_by_bsr_no(self, product_pane):
         """
-        filters the product by making sure it falls into the allowed range of brn numbers (1 - 150,000)
+        filters the product by making sure it falls into the allowed range of bsr ranking (1 - 150,000)
         if it passes return 1, otherwise 0 (helium extension used)
         """
         helium_pane = self.get_helium_pane(product_pane)
-        brns= []
-        for i in range(len(helium_pane.findAll('div', recursive= False)[0].findAll('div'))):
-            brn_data = (helium_pane.findAll('div', recursive= False)[0].findAll('div')[i].text)
+        for i in range(len(helium_pane.findAll('div', recursive= False)[0].findAll('div', recursive = False))):
+            bsr_data = helium_pane.findAll('div', recursive= False)[0].findAll('div', recursive = False)[i].text
             try:
-                brn_data = brn_data.split()[0][1:].replace(',','')
+                bsr_data = bsr_data.split()[0][1:].replace(',','')
             except IndexError:
-                print(IndexError)
+                print('err: ' + bsr_data)
                 return 0
             try:
-                if int(brn_data) > 150000:
-                    return 0
-                else:
-                    brns.append(int(brn_data))
+                if int(bsr_data) > 150000:
+                    return 0      
             except ValueError:
+                print('err: ' + bsr_data)
                 return 0
         return 1
     
@@ -224,7 +242,7 @@ class AmazonScrapper:
         result = []
         result.append(self.filter_asin_by_seller_count(product_pane))
         result.append(self.filter_asin_by_disallowed_brands(product_pane))
-        result.append(self.filter_asin_by_brn_no(product_pane))
+        result.append(self.filter_asin_by_bsr_no(product_pane))
         result.append(self.filter_asin_by_price(product_pane))
         return result
     
@@ -242,41 +260,60 @@ class AmazonScrapper:
                                       self.get_product_brand(i),
                                       self.get_product_link(i), 
                                       self.get_product_price(i),
-                                      self.get_ccc_link(i)[0],
-                                      self.get_ccc_link(i)[1]))
+#                                       self.get_ccc_links(i)[0],
+#                                       self.get_ccc_links(i)[1],
+                                      self.get_keepa_link(i)
+                                      ))
         return valid_products
     
     
-    def scrape_pages(self, no_of_products= 42):
+    def scrape_pages(self, no_of_products= 100):
         """
         Getting the qualified products after applying filters for multiple pages
         
         """
         try:
             products = []
-            for i in range(1, self.no_of_pages + 1):
+            for i in range(1, self.no_of_pages, 1):
                 if i == 1:
-                    self.set_src()
-                elif i > 1: 
-                    self.set_src(i)
+                    src = self.set_src()
+                    if src == '':
+                        continue            
+                elif i > 1:
+                    src = self.set_src(i)
+                    if src  == '':
+                        continue
+                        
                 panes = self.product_panes
                 product = self.apply_filters_on_page(panes)
                 products += product
-                print(f'Page {i} ==> {len(products)} product(s) found')
+                print(f'Page {i:4} ==> {len(products):4} product(s) found')
 
                 if len(products) >= no_of_products:
                     break
+                if len(products) in range(10, 401, 10):
+                    self.products_df = pd.DataFrame(set(products), columns=['ASIN', 'Description', 'Brand', 'Link', 'Price', 'Total Duration'])
+                    self.products_df['Defects'] = np.nan
+                    self.products_df['price+10%'] = np.nan
+                    self.save_result()
 
-            self.products_df = pd.DataFrame(products, columns=['ASIN', 'Description', 'Brand', 'Link', 'Price', 'Total Duration', '3 months Duration'])
+            self.products_df = pd.DataFrame(set(products), columns=['ASIN', 'Description', 'Brand', 'Link', 'Price', 'Total Duration'])
+            self.products_df['Defects'] = np.nan
+            self.products_df['price+10%'] = np.nan
+            self.save_result()
             return self.products_df, i
+        
         except KeyboardInterrupt:
-            self.products_df = pd.DataFrame(products, columns=['ASIN', 'Description', 'Brand', 'Link', 'Price', 'Total Duration', '3 months Duration'])
+            self.products_df = pd.DataFrame(set(products), columns=['ASIN', 'Description', 'Brand', 'Link', 'Price', 'Total Duration'])
+            self.products_df['Defects'] = np.nan
+            self.products_df['price+10%'] = np.nan
+            self.save_result()
             return self.products_df, i
     
      
-    def save_result(self):
-        self.products_df.to_excel('C:/Users/Jimoh/Documents/GitHub/Scraper/docs/products.xlsx', index= False)      
-    
+    def save_result(self, loc = 'products_extra_to_seller_14th.xlsx'):
+        self.products_df.to_excel('C:/Users/Jimoh/Documents/GitHub/Scraper/docs/' + loc, index= False)
+        return 'success'
         
 base = 'https://www.amazon.com/s?bbn=16225007011&dc&i=computers-intl-ship&k=-thfjfd%20keyboards&qid=1626710483&ref=glow_cls&refresh=1&rh=p_6%3AATVPDKIKX0DER%2Cp_36%3A2000-&rnid=386442011&s=relevancerank'
 
